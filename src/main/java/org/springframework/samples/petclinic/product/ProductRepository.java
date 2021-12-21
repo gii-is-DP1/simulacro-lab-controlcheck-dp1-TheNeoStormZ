5660d9723d1474b8bfbe27ff9bb372cefece45d0
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2021-12-21 19:26:08 +0100

    Subida hasta E8 completa

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,13 +3,27 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product,Integer>{
+	
     List<Product> findAll();
+    
+    
+    @Query("SELECT p FROM ProductType p")
     List<ProductType> findAllProductTypes();
+	
+    
+    @Query("SELECT p FROM ProductType p WHERE p.name LIKE :name")
+    ProductType getProductType(@Param("name") String name);
+    
+    @Query("SELECT p FROM Product p WHERE p.price < :price")
+    List<Product> findByPriceLessThan(@Param("price") double price);
+	
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,11 +1,31 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@Table(name = "producttypes")
 @Getter
 @Setter
-public class ProductType {
-    Integer id;
+
+public class ProductType extends BaseEntity{
+	
+	
+	@Column(name = "name",unique=true)
+	@Size(min = 3, max = 50)
     String name;
+	
+	
+	
+	
 }
